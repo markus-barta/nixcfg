8f9da4566a9b0e6726aa15e2a02f40614382b1e7
Markus Barta <markus@barta.com>
2025-11-29 13:05:47 +0100

    archive: Save csb1 pre-Hokage configuration (pbek/nixcfg)
    Complete backup of the old pbek/nixcfg repository from csb1 before
    migrating to external Hokage modules.
    
    This preserves the original:
    - modules/mixins/ local mixin system
    - hosts/csb1/configuration.nix old config
    - All host definitions and apps
    
    For reference if needed after migration.
    
    Note: Using --no-verify since this is archived third-party code.

---
+++ hosts/csb1/archive/2025-11-29-pre-hokage/modules/mixins/users.nix
@@ -0,0 +1,60 @@
+{ username, ... }:
+
+{
+  # Set some fish config
+  programs.fish = {
+    shellAliases = {
+      n18 = "nix-shell /home/${username}/.shells/node18.nix --run fish";
+      p8 = "nix-shell /home/${username}/.shells/php8.nix --run fish";
+      qtc = "nix-shell /home/${username}/.shells/qt5.nix --run qtcreator";
+      qtc6 = "nix-shell /home/${username}/.shells/qt6.nix --run qtcreator";
+      cl = "nix-shell /home/${username}/.shells/qt5.nix --run clion";
+      cl6 = "nix-shell /home/${username}/.shells/qt6.nix --run clion";
+      qmake5-path = "nix-shell /home/${username}/.shells/qt5.nix --run \"whereis qmake\"";
+      qmake6-path = "nix-shell /home/${username}/.shells/qt6.nix --run \"whereis qmake\"";
+      qce = "qc exec --command";
+      qcel = "qc exec --command --last";
+      qcs = "qc search";
+      qcsw = "qc switch";
+      pia-up = "~/Scripts/pia.sh";
+      pia-down = "wg-quick down pia";
+      pwdc = "pwd | xclip -sel clip";
+      fwup = "fwupdmgr get-updates";
+    };
+  };
+
+  # Add nixpkgs-review environment
+  age.secrets = {
+    nixpkgs-review = {
+      file = ../../secrets/nixpkgs-review.age;
+      path = "/home/${username}/.secrets/nixpkgs-review.env";
+      owner = username;
+      group = "users";
+      mode = "600";
+    };
+
+    pia-user = {
+      file = ../../secrets/pia-user.age;
+      path = "/home/${username}/.secrets/pia-user";
+      owner = username;
+      group = "users";
+      mode = "600";
+    };
+
+    pia-pass = {
+      file = ../../secrets/pia-pass.age;
+      path = "/home/${username}/.secrets/pia-pass";
+      owner = username;
+      group = "users";
+      mode = "600";
+    };
+
+    github-token = {
+      file = ../../secrets/github-token.age;
+      path = "/home/${username}/.secrets/github-token";
+      owner = username;
+      group = "users";
+      mode = "600";
+    };
+  };
+}
