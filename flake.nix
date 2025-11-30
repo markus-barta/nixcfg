15df6b2115928e5930a0a5ad2845a0b28ae34038
Markus Barta <markus@barta.com>
2025-11-30 18:15:42 +0100

    chore: switch to nixos-25.05 stable for full binary cache coverage
    Tired of building brave, plasma, etc from source. Stable = cached!

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "pbek's machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Using stable (25.05) for reliable binary cache - everything is pre-built!
+    # To go back to bleeding edge: change to nixos-unstable (may require local builds)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
