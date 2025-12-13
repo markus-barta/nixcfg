b520890f55baa85d555b65c58ff40b2c5aac955f
Markus Barta <markus@barta.com>
2025-12-13 18:13:34 +0100

    feat(hsb0): Migrate to isolated repo mode for nixfleet agent

--- hosts/hsb0/configuration.nix
+++ hosts/hsb0/configuration.nix
@@ -489,7 +489,7 @@ in
     url = "https://fleet.barta.cm";
     interval = 10;
     tokenFile = "/run/agenix/nixfleet-token";
-    configRepo = "/home/mba/Code/nixcfg";
+    repoUrl = "https://github.com/markus-barta/nixcfg.git"; # Isolated repo mode
     user = "mba";
     location = "home";
     deviceType = "server";
