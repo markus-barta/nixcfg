82e5003438895560895bb4bd16ff14c0c02d0275
Markus Barta <markus@barta.com>
2026-01-31 11:19:09 +0100

    fix(hsb2): hard-disable disko and switch to native sd-image builder

--- hosts/hsb2/configuration.nix
+++ hosts/hsb2/configuration.nix
@@ -9,9 +9,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./disk-config.nix
     ../../modules/uzumaki
-    inputs.disko.nixosModules.disko
   ];
 
   # ============================================================================
