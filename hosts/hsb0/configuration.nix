afe311c83998aac915461bf8b8a849ab2b02363c
Markus Barta <markus@barta.com>
2026-02-13 22:47:10 +0100

    fix(hsb0): restic secrets mode 444 for docker access

--- hosts/hsb0/configuration.nix
+++ hosts/hsb0/configuration.nix
@@ -477,14 +477,12 @@ in
   # Restic Hetzner secrets (shared sub1)
   age.secrets.restic-hetzner-env = {
     file = ../../secrets/restic-hetzner-env.age;
-    mode = "400";
-    owner = "root";
+    mode = "444";
   };
 
   age.secrets.restic-hetzner-ssh-key = {
     file = ../../secrets/restic-hetzner-ssh-key.age;
-    mode = "400";
-    owner = "root";
+    mode = "444";
   };
 
   # ============================================================================
