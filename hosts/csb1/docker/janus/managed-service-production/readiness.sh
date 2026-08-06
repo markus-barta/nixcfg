2ba7c8f5a36339ce647a53691e8e44be7172e1d2
Markus Barta <markus@barta.com>
2026-08-06 07:19:35 +0200

    test(csb1): expect Janus envelope v1.176 pin

--- hosts/csb1/docker/janus/managed-service-production/readiness.sh
+++ hosts/csb1/docker/janus/managed-service-production/readiness.sh
@@ -195,7 +195,7 @@ declarative() {
   check_image_pin \
     go_release_pin \
     janus \
-    '^ghcr\.io/inspr-at/janus/janus-envelope:go-envelope-v1\.175@sha256:[0-9a-f]{64}$'
+    '^ghcr\.io/inspr-at/janus/janus-envelope:go-envelope-v1\.176@sha256:[0-9a-f]{64}$'
   check_image_pin \
     rust_release_pin \
     janus-managed-transactiond \
