95839b6945d3760ae52ed7bb01f808d7807c702b
Markus Barta <markus@barta.com>
2026-08-10 17:39:01 +0200

    PHAROS-202: roll fleet to Pharos 0.1.81

--- hosts/csb1/docker/compose-spec.nix
+++ hosts/csb1/docker/compose-spec.nix
@@ -1070,7 +1070,7 @@
     pharosd = {
       # Keep the readable release tag, but bind it to the verified immutable
       # linux/amd64 manifest used by both server and bundled beacon.
-      image = "ghcr.io/inspr-at/pharos/pharosd:0.1.80@sha256:89b6f238ea7e53a26cca3061b38639d70947a2f1d3b989e4b6488c06cfed1772";
+      image = "ghcr.io/inspr-at/pharos/pharosd:0.1.81@sha256:497e1e5466e87492502971b4d6d9c806ed94f7da02c487e7e11a5bca56263f2c";
       container_name = "pharosd";
       restart = "unless-stopped";
       init = true;
@@ -1198,7 +1198,7 @@
     # (uid 1000) to read the nixcfg checkout natively; git computes commits-behind.
     # (Interim container deploy; native musl Nix-module onboarding is PHAROS-6/7.)
     pharos-beacon = {
-      image = "ghcr.io/inspr-at/pharos/pharosd:0.1.80@sha256:89b6f238ea7e53a26cca3061b38639d70947a2f1d3b989e4b6488c06cfed1772";
+      image = "ghcr.io/inspr-at/pharos/pharosd:0.1.81@sha256:497e1e5466e87492502971b4d6d9c806ed94f7da02c487e7e11a5bca56263f2c";
       container_name = "pharos-beacon";
       restart = "unless-stopped";
       init = true;
