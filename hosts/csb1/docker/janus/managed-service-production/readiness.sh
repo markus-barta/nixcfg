40f75c92937db37a155e4d603f171d731cf05e55
Markus Barta <markus@barta.com>
2026-09-01 08:12:39 -0700

    PHAROS-90: roll fleet to Pharos 26.09.01.13.29.31

--- hosts/csb1/docker/janus/managed-service-production/readiness.sh
+++ hosts/csb1/docker/janus/managed-service-production/readiness.sh
@@ -203,7 +203,7 @@ declarative() {
   check_image_pin \
     pharos_release_pin \
     pharosd \
-    '^ghcr\.io/inspr-at/pharos/pharosd:0\.2\.0@sha256:[0-9a-f]{64}$'
+    '^ghcr\.io/inspr-at/pharos/pharosd:26\.09\.01\.13\.29\.31@sha256:[0-9a-f]{64}$'
 
   if [ "$(service_image pharosd)" = "$(service_image pharos-beacon)" ]; then
     pass pharos_fleet_single_pin
