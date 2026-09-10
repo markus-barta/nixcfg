407b36e42b5e781ca10e22aad24d5d46add6698c
Markus Barta <markus@barta.com>
2026-09-10 09:17:20 +0200

    chore(csb0): bind final JoeDesk and navigation source revisions
    Signed-off-by: Markus Barta <markus@barta.com>

--- flake.nix
+++ flake.nix
@@ -50,11 +50,11 @@
     };
     # NIX-457: JoeDesk owns the application; HostDash owns service navigation.
     joedesk = {
-      url = "github:markus-barta/joedesk/0318cde78eeac46b84e86dfa2ccafd0a87b99de7";
+      url = "github:markus-barta/joedesk/af62be18e906e29ed97111e0eca72a8290e3b9cc";
       flake = false;
     };
     hostdash = {
-      url = "github:markus-barta/hostdash";
+      url = "github:markus-barta/hostdash/cd6680495e580e31e7f4a74af8ff7234e8986ac6";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     janus = {
