210fd00f82fc13ca1b2f17dedc01e7e7c7b82229
Markus Barta <markus@barta.com>
2026-09-12 15:33:44 +0200

    PHAROS-90: roll fleet to Pharos 260912102405.0.0

--- hosts/csb1/docker/compose-spec.nix
+++ hosts/csb1/docker/compose-spec.nix
@@ -1112,7 +1112,7 @@ in
       # PHAROS-206: guarded delivery bridge and fixed Debian security snapshot.
       # Pin the verified release index containing the linux/amd64 image and
       # provenance. Upgrade the control plane before starting v6 beacons.
-      image = "ghcr.io/inspr-at/pharos/pharosd:260911173640.0.0@sha256:55f06d1abb4845ecc4b256ab87643037b95f6dffa56d638ad21449a032d59514";
+      image = "ghcr.io/inspr-at/pharos/pharosd:260912102405.0.0@sha256:ead3f6b215e0bd4abebfb1b365aa07c8a231e3ff6a13e0a2d433755366ac54d3";
       container_name = "pharosd";
       restart = "unless-stopped";
       init = true;
@@ -1255,7 +1255,7 @@ in
     # (uid 1000) to read the nixcfg checkout natively; git computes commits-behind.
     # (Interim container deploy; native musl Nix-module onboarding is PHAROS-6/7.)
     pharos-beacon = {
-      image = "ghcr.io/inspr-at/pharos/pharosd:260911173640.0.0@sha256:55f06d1abb4845ecc4b256ab87643037b95f6dffa56d638ad21449a032d59514";
+      image = "ghcr.io/inspr-at/pharos/pharosd:260912102405.0.0@sha256:ead3f6b215e0bd4abebfb1b365aa07c8a231e3ff6a13e0a2d433755366ac54d3";
       container_name = "pharos-beacon";
       restart = "unless-stopped";
       init = true;
