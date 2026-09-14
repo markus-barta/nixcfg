af702cbee6d0e5076624776e8c732a32a3fb4808
Markus Barta <markus@barta.com>
2026-09-14 02:15:33 +0200

    NIX-495: pin Pharos sequence 15 release
    Signed-off-by: Markus Barta <markus@barta.com>

--- hosts/csb1/docker/compose-spec.nix
+++ hosts/csb1/docker/compose-spec.nix
@@ -1138,7 +1138,7 @@ in
       # PHAROS-206: guarded delivery bridge and fixed Debian security snapshot.
       # Pin the verified release index containing the linux/amd64 image and
       # provenance. Upgrade the control plane before starting v6 beacons.
-      image = "ghcr.io/inspr-at/pharos/pharosd:260911173640.0.0@sha256:55f06d1abb4845ecc4b256ab87643037b95f6dffa56d638ad21449a032d59514";
+      image = "ghcr.io/inspr-at/pharos/pharosd:260913221718.0.0@sha256:7e2f559ec166cde25f1b2d016c278045e164e55b7b0ddce4ee5c41f2fed9b5ee";
       container_name = "pharosd";
       restart = "unless-stopped";
       init = true;
@@ -1281,7 +1281,7 @@ in
     # (uid 1000) to read the nixcfg checkout natively; git computes commits-behind.
     # (Interim container deploy; native musl Nix-module onboarding is PHAROS-6/7.)
     pharos-beacon = {
-      image = "ghcr.io/inspr-at/pharos/pharosd:260911173640.0.0@sha256:55f06d1abb4845ecc4b256ab87643037b95f6dffa56d638ad21449a032d59514";
+      image = "ghcr.io/inspr-at/pharos/pharosd:260913221718.0.0@sha256:7e2f559ec166cde25f1b2d016c278045e164e55b7b0ddce4ee5c41f2fed9b5ee";
       container_name = "pharos-beacon";
       restart = "unless-stopped";
       init = true;
